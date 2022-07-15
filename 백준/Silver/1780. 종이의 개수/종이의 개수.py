1ea1e23632450c5e74c7e30262f9f2d6b6edac1c
Dale, Han <82254758+moordo91@users.noreply.github.com>
2022-07-15 18:34:47 +0900

    [Silver II] Title: 종이의 개수, Time: 1556 ms, Memory: 165872 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,37 +1,24 @@
-import sys; ipt = sys.stdin.readline
+n = int(input())
+p = [list(map(int, input().split())) for _ in range(n)]
 
 ans = [0, 0, 0]
 
-def check(p, y, x, n):
-
+def sol(y, x, n):
+    
     for i in range(y, y+n):
         for j in range(x, x+n):
             if p[i][j] != p[y][x]:
-                return False
-
-    return True
-
-def sol(p, y, x, n):
-
-    if not check(p, y, x, n):
-        n = n // 3
-        for i in range(3):
-            for j in range(3):
-                sol(p, y + i*n, x + j*n, n)
-
+                for k in range(3):
+                    for l in range(3):
+                        sol(y + k*n//3, x + l*n//3, n//3)
+                return
+    if p[y][x] < 0:
+        ans[0] += 1
+    elif p[y][x] > 0:
+        ans[2] += 1
     else:
-        if p[y][x] < 0:
-            ans[0] += 1
-        elif p[y][x] > 0:
-            ans[2] += 1
-        else:
-            ans[1] += 1
+        ans[1] += 1
 
+sol(0, 0, n)
 
-n = int(ipt())
-p = [list(map(int, ipt().split())) for _ in range(n)]
-
-sol(p, 0, 0, n)
-
-for i in ans:
-    print(i)
+for i in ans: print(i)
