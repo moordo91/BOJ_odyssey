82ab9d9d6d726d5fdc87fd2d622b7ad5ffa43d40
Dale Han <moordo91@gmail.com>
2022-07-16 14:15:09 +0900

    [Gold V] Title: AC, Time: 276 ms, Memory: 162080 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,49 +1,37 @@
 from collections import deque
 import sys; gets = sys.stdin.readline
 
-t = int(gets())
+t = int(input())
 
 for _ in range(t):
-    cmd = deque(list(map(str, gets().strip())))
-    end_switch = False
-    rev_switch = 0
-    n = int(gets())
-    arr = gets().strip()
-    if n > 0:
-        arr = arr[1:-1].split(',')
-        arr = deque(arr)
-        while cmd:
-            if cmd.popleft() == 'D':
-                try:
-                    if rev_switch % 2 == 0:
-                        arr.popleft()
-                    else:
-                        arr.pop()
-                except IndexError:
-                    end_switch = True
-                    print("error")
-                    break
+    cmd = gets().rstrip()
+    n = int(input())
+    arr = gets().rstrip()[1:-1].split(',')
+    q = deque(arr)
+
+    r, f, b, = 0, 0, len(q)-1
+    flag = 0
+    if n == 0:
+        q = []
+        f = 0
+        b = 0
+
+    for i in cmd:
+        if i == 'R':
+            r += 1
+        elif i == 'D':
+            if len(q) < 1:
+                flag = 1
+                print("error")
+                break
             else:
-                rev_switch += 1
-    
-    else:
-        if 'D' in cmd:
-            print("error")
-            continue
+                if r % 2 == 0:
+                    q.popleft()
+                else:
+                    q.pop()
+    if flag == 0:
+        if r % 2 == 0:
+            print('[' + ','.join(q) + ']')
         else:
-            end_switch = True
-            print("[]")
-    
-    if end_switch:
-        continue
-    
-    arr = list(arr)
-    if rev_switch % 2 == 1:
-        arr.reverse()
-    if len(arr) > 0:
-        print('[', end='')
-        for i in range(len(arr)-1):
-            print(arr[i], end=',')
-        print(arr[-1], end=']\n')
-    else:
-        print("[]")
+            q.reverse()
+            print('[' + ','.join(q) + ']')
