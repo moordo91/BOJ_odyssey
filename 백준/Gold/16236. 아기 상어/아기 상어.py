82364710b0a2d95416dbb3bbf302d63dd17c1861
Dale Han <moordo91@gmail.com>
2022-07-18 16:35:03 +0900

    [Gold III] Title: 아기 상어, Time: 232 ms, Memory: 117096 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,70 +1,64 @@
-import sys; gets = sys.stdin.readline
 from collections import deque
+import sys; gets = sys.stdin.readline
 
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
-shark = 2
-time = 0
-start = (0, 0)
-feed_cnt = 0
-
 n = int(input())
 ocean = [list(map(int, gets().split())) for _ in range(n)]
 
-for y in range(n):
-    for x in range(n):
-        if ocean[y][x] == 9:
-            ocean[y][x] = 0
-            start = (y, x)
-    
-def sol(dest, start, n):
-    y, x = start
-    fish_list = []
-    vnd = [[-1 for _ in range(n)] for _ in range(n)] # 해당 칸을 방문했는지(Visited) 그리고(aNd) 방문했다면 얼마나 멀리 떨어진 곳인지(Distance) 나타내는 2차원 리스트
+def sol(y, x, n):
     q = deque([(y, x)])
+    
+    vnd = [[-1 for _ in range(n)] for _ in range(n)]
     vnd[y][x] = 0
 
-    while q and dest:
-        y, x = q.popleft()
+    prey = []
 
+    while q:
+        y, x = q.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
 
-            if ny < 0 or ny >= n or nx < 0 or nx >= n:
-                continue 
+            if not (0 <= nx < n and 0 <= ny < n and vnd[ny][nx] < 0):
+                continue
 
-            if vnd[ny][nx] < 0 and ocean[ny][nx] <= shark:
-                vnd[ny][nx] = vnd[y][x] + 1
+            if ocean[ny][nx] <= shark:
                 q.append((ny, nx))
-                if (ny, nx) in dest:
-                    fish_list.append((vnd[ny][nx], (ny, nx)))
-                    dest.remove((ny, nx))
+                vnd[ny][nx] = vnd[y][x] + 1
+                if 0 < ocean[ny][nx] < shark:
+                    prey.append((vnd[ny][nx], ny, nx))
+    
+    return sorted(prey, key=lambda p: (-p[0], -p[1], -p[2]))
 
-    return fish_list
+
+for i in range(n):
+    for j in range(n):
+        if ocean[i][j] == 9:
+            ocean[i][j] = 0
+            y, x = i, j
+
+shark = 2
+exp = 0
+t = 0
 
 while 1:
-    dest = []
-    for y in range(n):
-        for x in range(n):
-            if 0 < ocean[y][x] < shark:
-                dest.append((y, x))
-    if not dest:
-        print(time)
+    prey = sol(y, x, n)
+    if not prey:
         break
     
     else:
-        fish_list = sol(dest, start, n)
-        if not fish_list:
-            print(time)
-            break
-        target = min(fish_list)
-        ocean[target[1][0]][target[1][1]] = 0
-        time += target[0]
-        start = target[1]
-        feed_cnt += 1
-    
-    if feed_cnt == shark:
-        shark += 1
-        feed_cnt = 0
+        prey = prey.pop()
+
+        ocean[y][x] = 0
+        t += prey[0]
+        y, x = prey[1], prey[2]
+
+        exp += 1
+
+        if exp == shark:
+            shark += 1
+            exp = 0
+
+print(t)
