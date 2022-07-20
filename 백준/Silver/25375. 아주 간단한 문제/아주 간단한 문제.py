f1699517c379d0ae58ac10eecba1df46ea0ef71b
Dale Han <moordo91@gmail.com>
2022-07-20 09:05:59 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 224 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.py
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.py
@@ -1,6 +1,3 @@
 import sys
-
 input()
-for line in sys.stdin:
-    a, b = map(int, line.split())
-    print(0 if b % a or a == b else 1)
+for l in sys.stdin:a,b=map(int,l.split());print(0if b%a or a==b else 1)
