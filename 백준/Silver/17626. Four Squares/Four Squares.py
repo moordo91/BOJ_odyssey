f042fa626c2dcab44fda504860056dd174454bc6
Dale Han <moordo91@gmail.com>
2022-07-24 05:31:55 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 116992 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,14 +1,11 @@
 n = int(input())
 l = [0]
-
 for i in range(1, n+1):
+    m = float('inf')
     j = 1
-    min_l = float('inf')
-
     while j**2 <= i:
-        min_l = min(min_l, l[i - j**2])
+        m = min(m, l[i - j**2])
         j += 1
-    
-    l.append(min_l + 1)
+    l.append(m+1)
 
 print(l[n])
