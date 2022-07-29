c3aac07f9701699b7a242c45c732860ddf8cba2c
Dale Han <moordo91@gmail.com>
2022-07-29 14:38:18 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 117152 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = []
+
+def sol(start):
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        if i not in arr:
+            arr.append(i)
+            sol(i+1)
+            arr.pop()
+
+sol(1)
