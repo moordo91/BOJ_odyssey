ed92416fae48e0cabe7999b7b6dec533cb514fdb
Dale Han <moordo91@gmail.com>
2022-08-08 16:15:25 +0900

    [Gold IV] Title: 최단경로, Time: 540 ms, Memory: 140880 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -2,43 +2,40 @@ import heapq
 import sys; gets = sys.stdin.readline
 INF = float('inf')
 
-
 def dijkstra(n, start, graph):
     heap = []
-    result = [INF] * n
-    result[start] = 0
+    cost_arr = [INF] * n
+    cost_arr[start] = 0
     heapq.heappush(heap, (0, start))
 
     while heap:
         d, now = heapq.heappop(heap)
 
-        if result[now] < d:
+        if cost_arr[now] < d:
             continue
 
-        for v, w in graph[now]:
+        for w, nxt in graph[now]:
             cost = w + d
 
-            if cost < result[v]:
-                result[v] = cost
-                heapq.heappush(heap, (cost, v))
-
-    return result
-
+            if cost < cost_arr[nxt]:
+                cost_arr[nxt] = cost
+                heapq.heappush(heap, (cost, nxt))
+    
+    return cost_arr
 
 n, e = map(int, gets().split())
-
 k = int(gets()) - 1
 
 graph = [[] for _ in range(n)]
+
 for _ in range(e):
     u, v, w = map(int, gets().split())
-    u, v= u-1, v-1
-    graph[u].append((v, w))
+    u, v = u-1, v-1
+    graph[u].append((w, v))
 
+cost_arr = dijkstra(n, k, graph)
 
-result = dijkstra(n, k, graph)
-
-for i in result:
+for i in cost_arr:
     if i == INF:
         print('INF')
     else:
