1f5ee094f92da39b0e8d41148f8b5357a529246a
Dale Han <moordo91@gmail.com>
2022-08-09 09:04:36 +0900

    [Silver III] Title: N과 M (5), Time: 220 ms, Memory: 117124 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+result = []
+
+def sol():
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+
+    for i in range(n):
+        if arr[i] not in result:
+            result.append(arr[i])
+            sol()
+            result.pop()
+
+sol()
