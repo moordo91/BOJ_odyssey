0d7751e92f4ec64e6f813ddcae1b6e4745bcc61c
Dale Han <moordo91@gmail.com>
2022-08-17 19:08:26 +0900

    [Gold IV] Title: 트리의 지름, Time: 96 ms, Memory: 43816 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -4,26 +4,25 @@ sys.setrecursionlimit(10**9)
 n = int(gets())
 graph = [[] for _ in range(n+1)]
 
-def dfs(graph, distance, start, weight):
+def dfs(graph, distance, start):
     for v_w in graph[start]:
         v, w = v_w
         if distance[v] == -1:
-            distance[v] = weight + w
-            dfs(graph, distance, v, distance[v])
+            distance[v] = distance[start] + w
+            dfs(graph, distance, v)
 
-
-for _ in range(n - 1):
+for _ in range(n-1):
     u, v, w = map(int, gets().split())
     graph[u].append((v, w))
     graph[v].append((u, w))
 
 dist = [-1] * (n+1)
 dist[1] = 0
-dfs(graph, dist, 1, 0)
-
+dfs(graph, dist, 1)
 farthest = dist.index(max(dist))
+
 dist = [-1] * (n+1)
 dist[farthest] = 0
-dfs(graph, dist, farthest, 0)
+dfs(graph, dist, farthest)
 
 print(max(dist))
