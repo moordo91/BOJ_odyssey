85b40f04cf9589a2b94bfee1448170c78ac0f14e
Dale Han <moordo91@gmail.com>
2022-08-19 10:10:23 +0900

    [Gold II] Title: 후위 표기식, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,27 @@
+m_e = list(input())
+stk = []
+ans = ""
+
+for i in m_e:
+    if i.isalpha():
+        ans += i
+    else:
+        if i == '(':
+            stk.append(i)
+        elif i =='*' or i == '/':
+            while stk and (stk[-1] == '*' or stk[-1] == '/'):
+                ans += stk.pop()
+            stk.append(i)
+        elif i == '+' or i == '-':
+            while stk and stk[-1] != '(':
+                ans += stk.pop()
+            stk.append(i)
+        else:
+            while stk and stk[-1] != '(':
+                ans += stk.pop()
+            stk.pop()
+            
+while stk:
+    ans += stk.pop()
+
+print(ans)
