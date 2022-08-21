8b4f0b8b61977272fae66e193e97cbfd910090b1
Dale Han <moordo91@gmail.com>
2022-08-22 01:48:27 +0900

    [Gold II] Title: 피보나치 수 3, Time: 12 ms, Memory: 7880 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+const int mod = 1000000;
+const int bnd = mod/10*15;
+
+int dp[bnd] = {0,1};
+int main() {
+    long long n;
+    cin >> n;
+    for (int i=2; i<bnd; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] %= mod;
+    }
+    cout << dp[n%bnd] << '\n';
+    return 0;
+}
