38d971f5bfedc4612de931ae9ffc1d32c84742d4
Dale Han <moordo91@gmail.com>
2022-09-06 16:25:53 +0900

    [Silver II] Title: 연속합, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -3,27 +3,27 @@
 
 using namespace std;
 
-int main() {
-    int n, i;
-    int ans = 0;
+int arr[100000];
+int dp[100000];
+
+int main()
+{
+    int n, ans;
 
     cin >> n;
-    int arr[n];
-    int dp[n];
-
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
 
+    ans = arr[0];
     dp[0] = arr[0];
-    ans = dp[0];
 
-    for (i = 1; i < n; i++) {
-        dp[i] = max(dp[i-1] + arr[i], arr[i]);
-        ans = max(ans, dp[i]);
+    for (int i = 1; i < n; i++)
+    {
+        dp[i] = max(arr[i], dp[i-1]+arr[i]);
+        ans = max(dp[i], ans);
     }
 
-    cout << ans;
+    cout << ans << endl;
 
     return 0;
-    
 }
