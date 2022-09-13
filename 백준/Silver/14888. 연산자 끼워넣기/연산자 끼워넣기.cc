9bef43bb1c7344928cf5d91064444096b62427e4
Dale Han <moordo91@gmail.com>
2022-09-13 13:07:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#define MAX 1000000000
+
+using namespace std;
+
+int N;
+int arr[11] = {0,};
+int oper[4] = {0,};
+
+int minVal = MAX;
+int maxVal = -MAX;
+
+void sol(int result, int idx) {
+    
+    if (idx == N) {
+        if (result < minVal)
+            minVal = result;
+        if (result > maxVal)
+            maxVal = result;
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+
+        if (oper[i] > 0) {
+            oper[i]--;
+            if (i == 0)
+                sol(result + arr[idx], idx+1);
+            else if (i == 1)
+                sol(result - arr[idx], idx+1);
+            else if (i == 2)
+                sol(result * arr[idx], idx+1);
+            else
+                sol(result / arr[idx], idx+1);
+            oper[i]++;
+        }
+    }
+    return;    
+}
+
+int main() {
+    
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < 4; i++)
+        cin >> oper[i];
+
+    sol(arr[0], 1);
+    cout << maxVal << endl
+         << minVal << endl;
+
+    return 0;
+}
