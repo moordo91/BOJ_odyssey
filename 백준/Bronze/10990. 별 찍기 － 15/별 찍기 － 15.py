e503a4d00a830849871108e1b087cc5c6d1a285a
Dale Han <moordo91@gmail.com>
2022-11-08 20:46:31 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 120 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+print(" "*(n-1) + "*")
+for i in range(1, n):
+    print(" "*(n-i-1), end='')
+    print("*", end='')
+    print(" "*(2*i-1), end='')
+    print("*", end='')
+    print()
