92d78e2bce3f784fe11a597dae1b2fd57132488d
Dale Han <moordo91@gmail.com>
2024-02-16 23:30:53 +0900

    [Silver I] Title: IOIOI, Time: 224 ms, Memory: 167704 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,20 +1,36 @@
-import sys; gets = sys.stdin.readline
-n = int(gets())
-m = int(gets())
-s = gets().strip()
+import sys
+input = sys.stdin.readline
 
-if 2*n+1 > m:
-    print(0)
-    exit(0)
+n = int(input())
+m = int(input())
+s = input()
+target = 'I' + 'OI'*n
 
-i, stk, cnt = 0, 0, 0
-while m - 2 >= i:
-    if s[i:i+3] == "IOI":
-        i += 2
-        stk += 1
-    else:
-        if stk >= n:
-            cnt += stk - n + 1
-        stk = 0
-        i += 1
-print(cnt)
+def get_pi(target):
+    pi = [0] * len(target)
+    target = list(target)
+    j = 0
+    for i in range(1, len(target)):
+        while j > 0 and target[i] != target[j]:
+            j = pi[j-1]
+        if target[i] == target[j]:
+            j += 1
+            pi[i] = j
+    return pi
+
+def kmp(s, target):
+    pi = get_pi(target)
+    j = 0
+    cnt = 0
+    for i in range(len(s)):
+        while j > 0 and s[i] != target[j]:
+            j = pi[j-1]
+        if s[i] == target[j]:
+            if j == len(target)-1:
+                cnt += 1
+                j = pi[j]
+            else:
+                j += 1
+    return cnt
+
+print(kmp(s, target))
