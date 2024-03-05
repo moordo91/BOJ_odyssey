baa93f0db0c1ed4d459aebf7fbd6e8cefd4ff6c0
Dale Han <moordo91@gmail.com>
2024-03-05 15:06:05 +0900

    [Gold III] Title: 치즈, Time: 224 ms, Memory: 115244 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -2,30 +2,29 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-grid = [list(map(int,input().split())) for _ in range(n)]
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
-def bfs():
+def bfs(visited, n, m):
     queue = deque([(0, 0)])
-    visited[0][0] = 1
+    dy = [0, 0, 1, -1]
+    dx = [1, -1, 0, 0]
     while queue:
-        x,y = queue.popleft()
+        y, x = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and visited[nx][ny] == 0:
-                if grid[nx][ny] >= 1:
-                    grid[nx][ny] += 1
+            ny, nx = y + dy[i], x + dx[i]
+            if 0<=ny<n and 0<=nx<m and visited[ny][nx] == 0:
+                if grid[ny][nx] >= 1:
+                    grid[ny][nx] += 1
                 else:
-                    visited[nx][ny] = 1
-                    queue.append((nx,ny))
+                    visited[ny][nx] = 1
+                    queue.append((ny, nx))
+
 answer = 0
-while 1:
-    visited = [[0]*m for _ in range(n)]
-    bfs()
+
+while True:
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+    bfs(visited, n, m)
     flag = 0
     for i in range(n):
         for j in range(m):
@@ -34,7 +33,7 @@ while 1:
                 flag = 1
             elif grid[i][j] == 2:
                 grid[i][j] = 1
-    if flag == 1:
+    if flag:
         answer += 1
     else:
         break
