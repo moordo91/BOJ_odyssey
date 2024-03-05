a05ec3b985a88039e7556f7a9cd3208f0fb7f183
Dale Han <moordo91@gmail.com>
2024-03-05 14:59:31 +0900

    [Gold III] Title: 치즈, Time: 228 ms, Memory: 114940 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -2,47 +2,41 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-# def visual (List):
-#     for i in range(n):
-#         print(List[i])
-
 n,m = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
+grid = [list(map(int,input().split())) for _ in range(n)]
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
-
 def bfs():
-    q = deque()
-    q.append([0,0])
+    queue = deque([(0, 0)])
     visited[0][0] = 1
-    while q:
-        x,y = q.popleft()
+    while queue:
+        x,y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0<=nx<n and 0<=ny<m and visited[nx][ny] == 0:
-                if graph[nx][ny] >= 1:
-                    graph[nx][ny] += 1
+                if grid[nx][ny] >= 1:
+                    grid[nx][ny] += 1
                 else:
                     visited[nx][ny] = 1
-                    q.append([nx,ny])
-time = 0
+                    queue.append((nx,ny))
+answer = 0
 while 1:
     visited = [[0]*m for _ in range(n)]
     bfs()
     flag = 0
     for i in range(n):
         for j in range(m):
-            if graph[i][j] >= 3:
-                graph[i][j] = 0
+            if grid[i][j] >= 3:
+                grid[i][j] = 0
                 flag = 1
-            elif graph[i][j] == 2:
-                graph[i][j] = 1
+            elif grid[i][j] == 2:
+                grid[i][j] = 1
     if flag == 1:
-        time += 1
+        answer += 1
     else:
         break
 
-print(time)
+print(answer)
