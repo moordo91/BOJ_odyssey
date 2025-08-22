26231ea8f498bb444d093b24a7006e8d6428255f
Dale Han <moordo91@gmail.com>
2025-08-22 19:46:15 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,18 @@
-from collections import deque
-import copy
+import math
+from collections import defaultdict
 
 def solution(progresses, speeds):
-    answer = []
-    chk = [False] * len(progresses)
-    while sum(answer) < len(progresses):
-        for i, (p, s) in enumerate(zip(progresses, speeds)):
-            progresses[i] = p + s
-        cnt = 0
-        for p in progresses:
-            if p >= 100:
-                cnt += 1
-            else:
-                break
-        
-        if cnt > sum(chk):
-            answer.append(cnt - sum(chk))
-        for i in range(cnt):
-            chk[i] = True
-    return answer
+    days_left = [math.ceil((100 - p) / s) for p, s in zip(progresses, speeds)]
+    
+    count = [1]
+    max_day = days_left[0]
+    day_dict = defaultdict(int)
+    for i in range(1, len(days_left)):
+        if max_day >= days_left[i]:
+            count.append(count[-1])
+        else:
+            max_day = days_left[i]
+            count.append(count[-1]+1)
+    for c in count:
+        day_dict[c] += 1
+    return [v for _, v in day_dict.items()]
