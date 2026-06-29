f43ec56e7a0c08a1cbcc310b261cf7b74dd6a90f
Dale Han <moordo91@gmail.com>
2026-06-30 01:03:34 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 1.41 ms, Memory: 4.96 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    int n, i;
+    cin >> str >> n;
+    for (i=0;i<n;i++) {
+        cout << str;
+    }
+    return 0;
+}
